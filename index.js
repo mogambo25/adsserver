8634c382f39c0c92f3b28d350197c558f41718bf
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-08-18 06:52:48 -0700

    Initial commit

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
-require('child_process').spawn('sh', ['migration.sh'], {stdio: 'inherit'});
+const spawn = require('child_process').spawn;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended: true}))
-  .use(require('./controllers'))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(require('./controllers'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const migration = spawn('sh', ['migration.sh'], {stdio: 'pipe'});
+migration.stdout.on('data', data => {
+  console.log('' + data);
+});
+migration.on('close', () => {
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+});
