751cd92b6f5e5493c8935cbdb4772222341639e1
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-08-18 07:43:51 -0700

    Test

--- controllers/login/index.js
+++ controllers/login/index.js
@@ -3,6 +3,7 @@ var express = require('express')
 var db = require('../../storage/main/models/index');
 
 router.get('/', (req, res) => {
+  
   db.User.find().then((result) => {
     res.send(result);
   });
